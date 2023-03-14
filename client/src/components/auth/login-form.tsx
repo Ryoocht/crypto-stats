fea0c021be1506e5cda4c37df7f09670e800d8c7
Ryoocht <ryo1.t.zubu@gmail.com>
2023-03-14 22:28:40 +1000

    implemented: login and signup form

--- client/src/components/auth/login-form.tsx
+++ client/src/components/auth/login-form.tsx
@@ -1,18 +1,56 @@
 import { Button, TextField, Link as MuiLink } from '@mui/material'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('')
+  const [emailErrored, setEmailErrored] = useState(false)
+
+  const [password, setPassword] = useState('')
+  const [passwordErrored, setPasswordErrored] = useState(false)
+
+  const handleLogin = () => {
+    if (!email) {
+      setEmailErrored(true)
+    } else {
+      setEmailErrored(false)
+    }
+    if (!password) {
+      setPasswordErrored(true)
+    } else {
+      setPasswordErrored(false)
+    }
+  }
+
   return (
     <div className='flex justify-center items-center flex-col h-screen gap-8'>
       <h1 className='text-6xl'>Crypto Stats</h1>
       <div className='flex flex-col gap-2'>
-        <TextField label='Email' className='w-80' type='email' required />
-        <TextField label='Password' className='w-80' type='passowrd' required />
+        <TextField
+          label='Email'
+          className='w-80'
+          type='email'
+          required
+          helperText={emailErrored && 'Please enter a valid email'}
+          value={email}
+          error={emailErrored}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <TextField
+          label='Password'
+          className='w-80'
+          type='passowrd'
+          required
+          helperText={passwordErrored && 'Password may not be empty'}
+          value={password}
+          error={passwordErrored}
+          onChange={e => setPassword(e.target.value)}
+        />
         <Link to='/singup' className='justify-self-start self-start mt-2'>
           <MuiLink>Sing Up</MuiLink>
         </Link>
       </div>
-      <Button variant='contained' className='w-80'>
+      <Button variant='contained' className='w-80' onClick={handleLogin}>
         <span className='p-1'>Login</span>
       </Button>
     </div>
--- client/src/components/auth/signup-form.tsx
+++ client/src/components/auth/signup-form.tsx
@@ -1,18 +1,56 @@
 import { Button, TextField, Link as MuiLink } from '@mui/material'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignupForm = () => {
+  const [email, setEmail] = useState('')
+  const [emailErrored, setEmailErrored] = useState(false)
+
+  const [password, setPassword] = useState('')
+  const [passwordErrored, setPasswordErrored] = useState(false)
+
+  const handleSignup = () => {
+    if (!email) {
+      setEmailErrored(true)
+    } else {
+      setEmailErrored(false)
+    }
+    if (!password) {
+      setPasswordErrored(true)
+    } else {
+      setPasswordErrored(false)
+    }
+  }
+
   return (
     <div className='flex justify-center items-center flex-col h-screen gap-8'>
       <h1 className='text-6xl'>Crypto Stats</h1>
       <div className='flex flex-col gap-2'>
-        <TextField label='Email' className='w-80' type='email' required />
-        <TextField label='Password' className='w-80' type='passowrd' required />
+        <TextField
+          label='Email'
+          className='w-80'
+          type='email'
+          required
+          helperText={emailErrored && 'Please enter a valid email'}
+          value={email}
+          error={emailErrored}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <TextField
+          label='Password'
+          className='w-80'
+          type='passowrd'
+          required
+          helperText={passwordErrored && 'Password may not be empty'}
+          value={password}
+          error={passwordErrored}
+          onChange={e => setPassword(e.target.value)}
+        />
         <Link to='/login' className='justify-self-start self-start mt-2'>
           <MuiLink>Login</MuiLink>
         </Link>
       </div>
-      <Button variant='contained' className='w-80'>
+      <Button variant='contained' className='w-80' onClick={handleSignup}>
         <span className='p-1'>Sign Up</span>
       </Button>
     </div>
